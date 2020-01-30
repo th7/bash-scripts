a23e176a7c1d5ea457ca5fa9c774757230662429
Tyler Hartland <tylerhartland7@gmail.com>
2020-01-30 14:08:51 -0600

    work around not-closing issue with open -W by setting up to use subl directly

--- setup-git.bash
+++ setup-git.bash
@@ -1,7 +1,7 @@
 source ~/.secrets.sh
 
 git config --global --unset-all core.editor
-git config --global --add core.editor "open -W -n"
+git config --global --add core.editor "subl -wn"
 git config --global user.name "Tyler Hartland"
 git config --global user.email $TYLER_EMAIL
 git config --global push.default current
