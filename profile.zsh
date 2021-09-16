edc6da26bb7908b5bf123c7830ca490321c362fb
Tyler Hartland <tyler.hartland@upstart.com>
2021-09-16 14:56:07 -0500

    fix git completions for zsh

--- profile.zsh
+++ profile.zsh
@@ -1,3 +1,8 @@
 source ~/.secrets.sh
 source "$BASH_SCRIPTS_DIR/profile.sh"
 source "$BASH_SCRIPTS_DIR/prompt.zsh"
+
+zstyle ':completion:*:*:git:*' script "$BASH_SCRIPTS_DIR/git-completion.bash"
+fpath=("$BASH_SCRIPTS_DIR/zsh-functions" $fpath)
+autoload -Uz compinit
+compinit
